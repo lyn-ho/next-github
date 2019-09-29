6e9cac09ebe27392af7ec56162e31a88dd2cc955
Lyn Ho <lyn1107130806@gmail.com>
2019-09-30 00:29:00 +0800

    feat: getInitialProps
    getInitialProps 获取数据的方式

--- pages/a.js
+++ pages/a.js
@@ -1,10 +1,24 @@
 import { withRouter } from 'next/router'
 import Link from 'next/link'
 
-const A = ({ router }) => (
+const A = ({ router, name }) => (
   <Link href="#aaa">
-    <a>A{router.query.id}</a>
+    <a>
+      A{router.query.id} {name}
+    </a>
   </Link>
 )
+
+A.getInitialProps = async () => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: 'tom',
+      })
+    }, 1000)
+  })
+
+  return await promise
+}
 
 export default withRouter(A)
