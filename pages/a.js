b7ebaafe7b6a8b9eb039bb0bbe02aecacaf72faa
Lyn Ho <lyn1107130806@gmail.com>
2019-09-30 01:44:17 +0800

    feat: lazy loading
    lazy loading module and component

--- pages/a.js
+++ pages/a.js
@@ -1,15 +1,20 @@
 import { withRouter } from 'next/router'
 import Link from 'next/link'
 import styled from 'styled-components'
+import dynamic from 'next/dynamic'
+// import moment from 'moment'
+
+const Comp = dynamic(import('../components/comp'))
 
 const Title = styled.h1`
   color: yellow;
   font-size: 23px;
 `
 
-const A = ({ router, name }) => (
+const A = ({ router, name, time }) => (
   <>
-    <Title>This is title</Title>
+    <Title>This is title {time}</Title>
+    <Comp />
     <Link href="#aaa">
       <a className="link">
         A{router.query.id} {name}
@@ -27,10 +32,13 @@ const A = ({ router, name }) => (
 )
 
 A.getInitialProps = async (ctx) => {
+  const moment = await import('moment')
+
   const promise = new Promise((resolve) => {
     setTimeout(() => {
       resolve({
         name: 'tom',
+        time: moment.default(Date.now() - 60 * 1000).fromNow(),
       })
     }, 1000)
   })
