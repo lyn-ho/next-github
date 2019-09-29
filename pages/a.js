c1ba7a4fd06b979c7d417598f126fe4a4ce8f1bc
Lyn Ho <lyn1107130806@gmail.com>
2019-09-30 01:34:41 +0800

    feat: css-in-js, styled component
    css in js , styled component

--- pages/a.js
+++ pages/a.js
@@ -1,8 +1,15 @@
 import { withRouter } from 'next/router'
 import Link from 'next/link'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: yellow;
+  font-size: 23px;
+`
 
 const A = ({ router, name }) => (
   <>
+    <Title>This is title</Title>
     <Link href="#aaa">
       <a className="link">
         A{router.query.id} {name}
