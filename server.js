b0dd431a3a35be2f3c475bea0408f67726fd4f46
Lyn Ho <lyn1107130806@gmail.com>
2019-09-29 23:34:56 +0800

    feat: add koa
    add koa and koa-router, use .npmrc and .nvmrc

---
+++ server.js
@@ -0,0 +1,23 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = new Koa()
+  const router = new Router()
+
+  server.use(async (ctx, _next) => {
+    await handle(ctx.req, ctx.res)
+    ctx.respond = false
+  })
+
+  server.use(router.routes())
+
+  server.listen(3000, () => {
+    console.log('koa server listening on 3000')
+  })
+})
