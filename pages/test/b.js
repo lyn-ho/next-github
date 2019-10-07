692466368d4a9eb4211a117a143585c8ca7966f0
Lyn Ho <lyn1107130806@gmail.com>
2019-10-07 23:34:55 +0800

    feat: effect hook
    effect hook, useEffect and useLayoutEffect

--- pages/test/b.js
+++ pages/test/b.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React, { useState, useEffect, useReducer, useLayoutEffect } from 'react'
 
 function countReducer(state, action) {
   switch (action.type) {
@@ -15,21 +15,39 @@ function MyCountFunc() {
   // const [count, setCount] = useState(0)
 
   const [count, dispatchCount] = useReducer(countReducer, 0)
+  const [name, setName] = useState('lyn')
 
   // setCount(1)
   // setCount(count + 1)  // 闭包陷阱，
   // setCount((c) => c + 1)
 
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     // setCount((c) => c + 1)
+  //     dispatchCount({ type: 'add' })
+  //   }, 1000)
+
+  //   return () => clearInterval(interval)
+  // }, [])
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      // setCount((c) => c + 1)
-      dispatchCount({ type: 'add' })
-    }, 1000)
+    console.log('effect invoked')
 
-    return () => clearInterval(interval)
-  }, [])
+    return () => console.log('effect detached')
+  }, [count]) // [], [name, count]  dependencies
 
-  return <h2>{count}</h2>
+  useLayoutEffect(() => {
+    console.log('layout effect invoked')
+
+    return () => console.log('layout effect detached')
+  }, [count]) // [], [name, count]  dependencies
+
+  return (
+    <div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => dispatchCount({ type: 'add' })}>{count}</button>
+    </div>
+  )
 }
 
 export default MyCountFunc
