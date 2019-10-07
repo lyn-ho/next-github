bb8ab7f57c57641bb227a30e3bef28f2ecac0a02
Lyn Ho <lyn1107130806@gmail.com>
2019-10-07 23:13:14 +0800

    feat: hooks
    hooks, useState and useEffect

--- pages/test/b.js
+++ pages/test/b.js
@@ -1,1 +1,17 @@
-export default () => <h2>B</h2>
+import React, { useState, useEffect } from 'react'
+
+function MyCountFunc() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((c) => c + 1)
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return <h2>{count}</h2>
+}
+
+export default MyCountFunc
