9e322a19244651601a2c4dd1bb0e5b36d7f801f4
Lyn Ho <lyn1107130806@gmail.com>
2019-10-07 23:22:39 +0800

    feat: state hook
    state hook, useReducer and useState

--- pages/test/b.js
+++ pages/test/b.js
@@ -1,11 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
+
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'add':
+      return state + 1
+    case 'minus':
+      return state - 1
+    default:
+      return state
+  }
+}
 
 function MyCountFunc() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+
+  const [count, dispatchCount] = useReducer(countReducer, 0)
+
+  // setCount(1)
+  // setCount(count + 1)  // 闭包陷阱，
+  // setCount((c) => c + 1)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCount((c) => c + 1)
+      // setCount((c) => c + 1)
+      dispatchCount({ type: 'add' })
     }, 1000)
 
     return () => clearInterval(interval)
