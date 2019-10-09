58efbbd6f8041a068f45f727f463c0717734300d
Lyn Ho <lyn4develop@gmail.com>
2019-10-09 15:27:52 +0800

    feat: basic layout
    basic layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,84 @@
+import { useState, useCallback } from 'react'
 import Link from 'next/link'
-import { Button } from 'antd'
+import { Button, Layout, Icon, Input, Avatar } from 'antd'
 
-export default ({ children }) => (
-  <>
-    <header>
-      <Link href="/a?id=1" as="/a/1">
-        <Button>to a</Button>
-      </Link>
-      <Link href="/test/b">
-        <Button>to b</Button>
-      </Link>
-    </header>
-    {children}
-  </>
-)
+const { Header, Content, Footer } = Layout
+
+const githubIconStyle = {
+  color: 'white',
+  fontSize: 40,
+  display: 'block',
+  paddingTop: 10,
+  marginRight: 20,
+}
+
+const footerStyle = {
+  textAlign: 'center',
+}
+
+export default ({ children }) => {
+  const [search, setSearch] = useState('')
+
+  const handleSearchChange = useCallback(
+    (evt) => {
+      setSearch(evt.target.value)
+    },
+    [setSearch]
+  )
+
+  const handleOnSearch = useCallback(() => {}, [])
+
+  return (
+    <Layout>
+      <Header>
+        <div className="header-inner">
+          <div className="header-left">
+            <div className="logo">
+              <Icon type="github" style={githubIconStyle} />
+            </div>
+            <div>
+              <Input.Search
+                placeholder="搜索仓库"
+                value={search}
+                onChange={handleSearchChange}
+                onSearch={handleOnSearch}
+              />
+            </div>
+          </div>
+          <div className="header-right">
+            <div className="user">
+              <Avatar size={40} icon="user" />
+            </div>
+          </div>
+        </div>
+      </Header>
+      <Content>{children}</Content>
+      <Footer style={footerStyle}>
+        Develop by Lyn @
+        <a href="mailto:lyn4develop@gmail.com">lyn4develop@gmail.com</a>
+      </Footer>
+
+      <style jsx>{`
+        .header-inner {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        .header-left {
+          display: flex;
+          justify-content: flex-start;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        #__next {
+          height: 100%;
+        }
+
+        .ant-layout {
+          height: 100%;
+        }
+      `}</style>
+    </Layout>
+  )
+}
---
+++ pages-test/_app.js
@@ -0,0 +1,42 @@
+import App, { Container } from 'next/app'
+import 'antd/dist/antd.css'
+import { Provider } from 'react-redux'
+
+import Layout from '../components/Layout'
+
+import withRedux from '../lib/with-redux'
+
+class MyApp extends App {
+  state = {
+    context: 'value',
+  }
+
+  static async getInitialProps(ctx) {
+    const { Component } = ctx
+    console.log('app init')
+
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps, reduxStore } = this.props
+
+    return (
+      <Container>
+        <Layout>
+          <Provider store={reduxStore}>
+            <Component {...pageProps} />
+          </Provider>
+        </Layout>
+      </Container>
+    )
+  }
+}
+
+export default withRedux(MyApp)
