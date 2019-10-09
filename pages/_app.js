ac144e430dba7cafe5b3db33d3b2836e813c6da6
Lyn Ho <lyn4develop@gmail.com>
2019-10-09 21:17:12 +0800

    feat: loading page
    add loading page while route change.

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,42 @@
 import App, { Container } from 'next/app'
 import 'antd/dist/antd.css'
 import { Provider } from 'react-redux'
+import Router from 'next/router'
+import Link from 'next/link'
 
 import Layout from '../components/Layout'
+import PageLoading from '../components/PageLoading'
 
 import withRedux from '../lib/with-redux'
 
 class MyApp extends App {
   state = {
     context: 'value',
+    loading: false,
+  }
+
+  startLoading = () => {
+    this.setState({
+      loading: true,
+    })
+  }
+
+  stopLoading = () => {
+    this.setState({
+      loading: false,
+    })
+  }
+
+  componentDidMount() {
+    Router.events.on('routeChangeStart', this.startLoading)
+    Router.events.on('routeChangeComplete', this.stopLoading)
+    Router.events.on('routeChangeError', this.stopLoading)
+  }
+
+  componentWillUnmount() {
+    Router.events.off('routeChangeStart', this.startLoading)
+    Router.events.off('routeChangeComplete', this.stopLoading)
+    Router.events.off('routeChangeError', this.stopLoading)
   }
 
   static async getInitialProps(ctx) {
@@ -30,7 +58,14 @@ class MyApp extends App {
     return (
       <Container>
         <Provider store={reduxStore}>
+          {this.state.loading ? <PageLoading /> : null}
           <Layout>
+            <Link href="/">
+              <a>Index</a>
+            </Link>
+            <Link href="/detail">
+              <a>Detail</a>
+            </Link>
             <Component {...pageProps} />
           </Layout>
         </Provider>
